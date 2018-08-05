eeac473dccb1c9aedef0fd5505a995597bc4f2dd
Corinne Hatton <claurelh@gmail.com>
2018-08-05 16:47:32 -0400

    Get it together

--- resources/js/jumpstart.js
+++ resources/js/jumpstart.js
@@ -1,1 +1,32 @@
 var Jumpstart = {};
+
+
+// This is the jQuery for the carousel
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  $(window).scroll(
+    {
+        previousTop: 0
+    },
+    function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $(".sidebar em").text("Up"); /* optional for demo */
+        $(".navbar").show();
+    } else {
+        $(".sidebar em").text("Down");
+        $(".navbar").hide();
+    }
+    this.previousTop = currentTop;
+});
+});
+// jQuery end
