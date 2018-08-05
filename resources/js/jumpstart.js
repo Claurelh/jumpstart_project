518bff0d6942257da5d689107ebf3e5fbf8f75f9
Corinne Hatton <claurelh@gmail.com>
2018-08-05 19:23:44 -0400

    Finished Project

--- resources/js/jumpstart.js
+++ resources/js/jumpstart.js
@@ -13,20 +13,17 @@ $(document).ready(function(){
     pauseOnHover: false
   });
 
-  $(window).scroll(
-    {
-        previousTop: 0
-    },
-    function () {
-    var currentTop = $(window).scrollTop();
-    if (currentTop < this.previousTop) {
-        $(".sidebar em").text("Up"); /* optional for demo */
-        $(".navbar").show();
-    } else {
-        $(".sidebar em").text("Down");
-        $(".navbar").hide();
-    }
-    this.previousTop = currentTop;
-});
-});
+  var scrollDown = 0 // aka variable "x" this variable tells how far the user will scroll down
+  $(window).scroll(function(){
+    var scrollLimit = $(this).scrollTop(); //aka variable "y", the .scroll() event handler selects the window
+    if (scrollLimit - scrollDown > 50){ //if y - x is greater than 50
+      var stopScroll = $('.navbar').css('height'); //stores the height of the navbar
+      $('.navbar').animate({top: '-' + stopScroll}, 150);
+      scrollDown = scrollLimit; //so there is a record of how far the user scrolled last time they finished scrolling
+    } else if (scrollDown - scrollLimit > 50) { // this if x - y is greater than 50 the navbar will be 0px
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollDown = scrollLimit;
+      }
+    });
+  });
 // jQuery end
